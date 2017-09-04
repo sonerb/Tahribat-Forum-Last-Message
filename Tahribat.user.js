f93edaa7177f92f3a71e83a18f6ca57d5d1f40cf
Soner <sonerb@users.noreply.github.com>
2017-09-04 21:00:48 +0300

    User Script added.

---
+++ Tahribat.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Tahribat Forum Son Mesaj
+// @namespace    http://www.tahribat.com/Murit-pSkpt?ref=39260
+// @version      1.0
+// @description  Anasayfada bulunan "Aktif Konular" kısmında son sayfaya gitmek için kullanılır.
+// @author       pSkpt
+// @match        *://www.tahribat.com/*
+// @grant        none
+// ==/UserScript==
+
+var image_url = 'https://raw.githubusercontent.com/sonerb/Tahribat-Forum-Last-Message/master/images/icon_last.png';
+
+if (location.pathname == '/') {
+
+    // Her bir aktif konu linkinin sonuna son konuya gitme linki ekleniyor.
+    $("table#activeTopics").find("a[href^='/forum']").each(function(index) {
+        $(this).after('&nbsp;&nbsp;<a href="' + $(this).attr("href") + '/0/#son"><img src="'+image_url+'" style="heigh:11px; width:11px" /></a>');
+    });
+
+} else if (location.pathname.substr(0, 6) == '/forum') {
+
+    // #son hash'i varsa sayfayı aşağı kaydırıyor.
+    if (window.location.hash === '#son') {
+        $("html, body").animate({
+            scrollTop: $(document).height() - $(window).height()
+        });
+    }
+}
